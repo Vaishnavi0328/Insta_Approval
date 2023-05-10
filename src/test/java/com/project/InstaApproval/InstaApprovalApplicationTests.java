39f097bf12626dece6dc7bfdb4241a4b3e4fd7ba
Vaishnavi Sanjay Khandade <V7113619@ntil.com>
2023-05-10 11:23:43 +0530

    Initial commit

---
+++ src/test/java/com/project/InstaApproval/InstaApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.InstaApproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
